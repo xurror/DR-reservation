3939ff752ed5a123d34f648753442286cd2f6299
xurror <kaze.nasser@outlook.com>
2019-10-04 16:23:48 +0100

    update seeders and migrations

--- database/seeds/ReservationsTableSeeder.php
+++ database/seeds/ReservationsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class ReservationsTableSeeder extends Seeder
 {
@@ -13,12 +14,18 @@ class ReservationsTableSeeder extends Seeder
     {
         $customer_ids = DB::table('customers')->pluck('id')->toArray();
         $room_ids = DB::table('rooms')->pluck('id')->toArray();
-        
+
+        $year = rand(2009, 2016);
+        $month = rand(1, 12);
+        $day = rand(1, 28);
+
+        $date = Carbon::create($year,$month ,$day , 0, 0, 0);
+
         DB::table('reservations')->insert([
             'customer_id' => $customer_ids[array_rand($customer_ids)],
             'room_id' => $room_ids[array_rand($room_ids)],
-            'reservation_date' => new DateTime(),
-            'expiry_date' => new DateTime(),
+            'reservation_date' => $date->format('Y-m-d H:i:s'),
+            'expiry_date' => $date->addWeeks(rand(1, 52))->format('Y-m-d H:i:s'),
             'status' => (bool)rand(0,2),
             'No_of_rooms' => rand(1, 10),
         ]);
